17bb3b76b28b3d240bde879869fa0932af334bb4
RafaelMF16 <rafaelmoreiraferreira63@gmail.com>
2024-12-14 15:38:09 -0300

    atualizacao para usar variaveis de ambiente

--- server.js
+++ server.js
@@ -1,28 +1,34 @@
+require('dotenv').config(); // Carrega variáveis de ambiente de um arquivo .env
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
+
+// Variáveis de ambiente
+const SECRET_KEY = process.env.SECRET_KEY || 'defaultsecretkey'; // Chave secreta
+const ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'admin'; // Nome de usuário
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'password123'; // Senha
+
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: 'http://localhost:4200', // Ajuste conforme necessário
   methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
+  credentials: true,
 };
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
-
 app.options('*', cors(corsOptions));
 
-// Configurações de autenticação
-const SECRET_KEY = 'faculdadepmsecretkey';
+// Hash da senha para o administrador
 const usuarios = [
-  { username: 'admin', password: bcrypt.hashSync('FPM1234', 10) },
+  { username: ADMIN_USERNAME, password: bcrypt.hashSync(ADMIN_PASSWORD, 10) },
 ];
 
 // Função para verificar token JWT
@@ -56,14 +62,24 @@ app.post('/login', (req, res) => {
 
 // Rota para obter confrontos
 app.get('/confrontos', (req, res) => {
-  const data = fs.readFileSync('confrontos.json');
-  res.send(JSON.parse(data));
+  try {
+    const data = fs.readFileSync('confrontos.json');
+    res.json(JSON.parse(data));
+  } catch (error) {
+    console.error('Erro ao ler confrontos:', error);
+    res.status(500).json({ message: 'Erro ao ler confrontos.' });
+  }
 });
 
 // Rota para atualizar confrontos (somente a professora)
 app.post('/confrontos', verificarToken, (req, res) => {
   try {
     const novosConfrontos = req.body;
+
+    // Validar a estrutura dos dados
+    if (!novosConfrontos.data || !Array.isArray(novosConfrontos.partidas)) {
+      return res.status(400).json({ message: 'Formato de dados inválido.' });
+    }
 
     // Salvar confrontos no arquivo
     fs.writeFileSync('confrontos.json', JSON.stringify({ confrontos: [novosConfrontos] }, null, 2));
